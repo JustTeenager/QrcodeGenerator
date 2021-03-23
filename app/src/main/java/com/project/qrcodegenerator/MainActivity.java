75cdec92e9356e8e1ada5fcd480cac14af1b375e
JustTeenager <56716766+JustTeenager@users.noreply.github.com>
2021-03-23 10:27:57 +0300

    Finality

--- app/src/main/java/com/project/qrcodegenerator/MainActivity.java
+++ app/src/main/java/com/project/qrcodegenerator/MainActivity.java
@@ -9,6 +9,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 import android.widget.ImageView;
@@ -18,6 +19,10 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,17 +37,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try {
+            qrCodeText = findViewById(R.id.uml_code_text);
+            qrCodeView = findViewById(R.id.qr_code_view);
 
-        qrCodeText = findViewById(R.id.uml_code_text);
-        qrCodeView = findViewById(R.id.qr_code_view);
-
-        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
-        if (permission == PackageManager.PERMISSION_GRANTED){
-            createQrCode();
+            int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE);
+            if (permission == PackageManager.PERMISSION_GRANTED) {
+                createQrCode();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_PERMISSION_CODE);
+            }
         }
-        else{
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_PHONE_STATE}, REQUEST_PERMISSION_CODE);
-        }
+        catch (Exception e){setDebuggableText(e);}
     }
 
     @Override
@@ -57,8 +63,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void createQrCode() {
         manager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-        qrCodeView.setImageBitmap(urlToImageEncode(startUml+manager.getDeviceId()));
-        qrCodeText.setText(startUml+manager.getDeviceId());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            qrCodeView.setImageBitmap(urlToImageEncode(startUml+ manager.getImei()));
+           // qrCodeText.setText(startUml + manager.getImei());
+        }
+        else {
+            qrCodeView.setImageBitmap(urlToImageEncode(startUml+manager.getDeviceId()));
+           // qrCodeText.setText(startUml+manager.getDeviceId());
+        }
     }
 
     private Bitmap urlToImageEncode(String uml) {
@@ -71,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             for (int y =0; y <bitMatrixHeight; y++){
                 int offset = y*bitMatrixWidth;
                 for (int x = 0; x < bitMatrixWidth; x++){
-                    pixels[offset + x] = bitMatrix.get(x,y) ? getResources().getColor(R.color.purple_700) : getResources().getColor(R.color.white);
+                    pixels[offset + x] = bitMatrix.get(x,y) ? getResources().getColor(R.color.qr_code_blue) : getResources().getColor(R.color.white);
                 }
             }
 
@@ -79,10 +91,17 @@ public class MainActivity extends AppCompatActivity {
             bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth,bitMatrixHeight);
             return bitmap;
         } catch (WriterException e) {
+            setDebuggableText(e);
             e.printStackTrace();
             return null;
         }
 
+    }
+
+    private void setDebuggableText(Exception e){
+        Writer writer = new StringWriter();
+        e.printStackTrace(new PrintWriter(writer));
+        qrCodeText.setText(writer.toString());
     }
 
 }
